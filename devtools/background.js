996cd58dc79a87031b46bf0b5153e61dd593b5de
Daniele Scasciafratte <mte90net@gmail.com>
2018-06-28 22:55:40 +0200

    fix #6

--- devtools/background.js
+++ devtools/background.js
@@ -1,9 +1,14 @@
-browser.runtime.onConnect.addListener(function (port) {
+if (typeof browser === "undefined") {
+    var background = chrome;
+} else {
+    var background = browser;
+}
+background.runtime.onConnect.addListener(function (port) {
   // Wait from communication from the panel
   port.onMessage.addListener(function (request, sender) {
 	if (request.type === "qm-div-load") {
 	  // Get the html panel
-	  browser.tabs.executeScript(request.tabId, {"code": "document.querySelector(\"#query-monitor\").outerHTML;"}, function (qm) {
+	  background.tabs.executeScript(request.tabId, {"code": "document.querySelector(\"#query-monitor\").outerHTML;"}, function (qm) {
 		if (typeof qm !== "undefined") {
 			// Pass the output of the QM html to panel
 			port.postMessage({
--- devtools/devtools.js
+++ devtools/devtools.js
@@ -1,7 +1,12 @@
 /**
  Create a panel, and add listeners for panel show/hide events.
  */
-browser.devtools.panels.create(
+if (typeof browser === "undefined") {
+    var contentscript = chrome;
+} else {
+    var contentscript = browser;
+}
+contentscript.devtools.panels.create(
 		"WP Query Monitor",
 		"icon.png",
 		"/devtools/panel/panel.html"
